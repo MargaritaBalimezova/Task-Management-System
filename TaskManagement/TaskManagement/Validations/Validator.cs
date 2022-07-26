fb07dfbdd4424d05502ebc824b4ef4c0beeceeef
Stefan Yotov <s.yotov27@gmail.com>
2022-07-26 16:36:51 +0300

    Structure and Validators

---
+++ TaskManagement/TaskManagement/Validations/Validator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class Validator
+{
+    public static void ValidateIntRange(int value, int min, int max, string message)
+    {
+        if (value < min || value > max)
+        {
+            throw new ArgumentException(message);
+        }
+    }
+
+    public static void ValidateDoubleRange(double value, double min, double max, string message)
+    {
+        if (value < min || value > max)
+        {
+            throw new ArgumentException(message);
+        }
+    }
+
+    public static void ValidateArgumentIsNotNull(object arg, string message)
+    {
+        if (arg == null)
+        {
+            throw new ArgumentException(message);
+        }
+    }
+}
