63cff6cd553d5a1f34cfd8cde0eed2f9bbf1a3e8
Radoslav Berov <rado561.nbr@gmail.com>
2022-07-28 08:59:03 +0300

    Implemented Class Bug and Interface IBug

---
+++ TaskManagement/TaskManagement/Models/Contracts/IBug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagement.Models.Enums;
+
+namespace TaskManagement.Models.Contracts
+{
+    internal interface IBug : ITask
+    {
+        public string Assignee { get; }
+
+        public PriorityType Priority { get; }
+
+        public Severity Severity { get; }
+        
+        public IList<string> StepsToReproduce { get; }
+    }
+}
