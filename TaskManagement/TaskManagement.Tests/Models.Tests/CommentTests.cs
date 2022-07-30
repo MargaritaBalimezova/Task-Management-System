eacdde21c6ea315d6ec7fbe004cbef7cb0500518
Radoslav Berov <rado561.nbr@gmail.com>
2022-07-30 17:09:41 +0300

    CommentTests Implemented

---
+++ TaskManagement/TaskManagement.Tests/Models.Tests/CommentTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagement.Models;
+
+namespace TaskManagement.Tests.Models.Tests
+{
+    [TestClass]
+    public class CommentTests
+    {
+        [TestMethod]
+        public void ConstructorShould_CreateNewComment_When_CorrectValuesArePassed()
+        {
+            string author = "Test Author";
+            string content = "This is a test description";
+            var comment = new Comment(content, author);
+
+            Assert.AreEqual("Test Author", comment.Author,"Comment created successfully!");
+            Assert.AreEqual("This is a test description", comment.Content, "Comment created successfully!");            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),"Input value can not be null!")]
+        public void ConstructorShould_Throw_When_NullValueIsPassedToTheAuthor()
+        {
+            string author = null;
+            string content = "This is a test description";
+            var comment = new Comment(content, author);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Input value can not be null!")]
+        public void ConstructorShould_Throw_When_NullValueIsPassedToTheContent()
+        {
+            string author = "Test Author";
+            string content = null;
+            var comment = new Comment(content, author);
+        }
+    }
+}
