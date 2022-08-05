9ae259c486988158dd35d4fc1d9fe3dd67e940c7
Angel Marinski <77447043+AngelMarinski@users.noreply.github.com>
2022-08-05 18:00:13 +0300

    add CreateTeam command tests

---
+++ TaskManagement/TaskManagement.Tests/Commands.Tests/CreateTeamTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagement.Commands;
+using TaskManagement.Core;
+using TaskManagement.Core.Contracts;
+
+namespace TaskManagement.Tests.Commands.Tests
+{
+    [TestClass]
+    public class CreateTeamTests
+    {
+        private const string teamName = "DummyTeam";
+        private const int ExpectedParamsCount = 1;
+
+        private IRepository repository;
+        private ICommandFactory commandFactory;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            this.repository = new Repository();
+            this.commandFactory = new CommandFactory(this.repository);
+        }
+
+        [TestMethod]
+        public void Execute_Should_CreateTeam_When_ParamsValid()
+        {
+            //Arrange
+            var commandParameters = new string[] { teamName };
+            var command = new CreateTeamCommand(commandParameters, repository);
+
+            //Act
+            command.Execute();
+
+            //Assert
+            Assert.AreEqual(teamName, this.repository.Teams[this.repository.Teams.Count - 1].Name,
+                $"CreateCommand failed to create a team with name {teamName}");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "CreateTeam parameters count validation failed!")]
+        public void Execute_Should_ThrowException_When_ArgumentCountDiffThanExpected()
+        {
+            //Arrange
+            var commandParameters = Helpers.GetDummyList(ExpectedParamsCount + 1);
+            var command = new CreateTeamCommand(commandParameters, repository);
+
+            //Act & Asssert
+            command.Execute();
+        }
+    }
+}
