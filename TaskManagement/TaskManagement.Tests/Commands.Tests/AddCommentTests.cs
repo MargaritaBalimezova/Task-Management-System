7b9bd0f9a85f6bf82c1f1fdc14c9882e0e1f33c9
Angel Marinski <77447043+AngelMarinski@users.noreply.github.com>
2022-08-06 14:52:47 +0300

    add AddComment and ShowAllTeams command tests

---
+++ TaskManagement/TaskManagement.Tests/Commands.Tests/AddCommentTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagement.Commands;
+using TaskManagement.Core;
+using TaskManagement.Core.Contracts;
+using TaskManagement.Models.Contracts;
+using TaskManagement.Models.Enums;
+
+namespace TaskManagement.Tests.Commands.Tests
+{
+    [TestClass]
+    public class AddCommentTests
+    {
+        private const int ExpectedParamsCount = 3;
+        private const string MemberName = "DummyMember";
+        private const string Comment = "this is dummy comment";
+
+        private IRepository repository;
+        private ICommandFactory commandFactory;
+        private string title;
+        private string description;
+        private PriorityType priority;
+        private SizeType size;
+        private IStory story;
+        private IMember member;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            this.repository = new Repository();
+            this.commandFactory = new CommandFactory(this.repository);
+            this.title = "StoryDummy";
+            this.description = "DescriptionDummy";
+            this.priority = PriorityType.High;
+            this.size = SizeType.Medium;
+
+            this.story = this.repository.CreateStory(title, description, priority, size);
+            this.member = this.repository.CreateMember(MemberName);
+        }
+
+        [TestMethod]
+        public void Execute_Should_AddCommentToTask()
+        {
+            //Arrange
+            var commandParams = new string[] {this.story.Id.ToString()
+                                               ,MemberName
+                                               ,Comment};
+
+            var command = new AddCommentCommand(commandParams, repository);
+
+            //Act
+            command.Execute();
+
+            //Assert
+            Assert.IsTrue(this.repository.Stories[this.repository.Stories.Count - 1].Comments.Count > 0,
+                "AddComment failed to add comment to task");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "AddComment id validation failed!")]
+        public void Execute_Should_ThrowException_When_TaskIdDoesNotExists()
+        {
+            //Arrange
+            var commandParams = new string[] {  "kuche"
+                                               ,MemberName
+                                               ,Comment};
+
+            var command = new AddCommentCommand(commandParams, repository);
+
+            //Act & Assert
+            command.Execute();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),"AddMemberToTeam parameters count validation failed!")]
+        public void Execute_Should_ThrowException_When_ArgumentCountDiffThanExpected()
+        {
+            //Arrange
+            var commandParameters = Helpers.GetDummyList(ExpectedParamsCount - 1);
+            var command = new AddCommentCommand(commandParameters, repository);
+
+            //Act & Asssert
+            command.Execute();
+        }
+    }
+}
---
+++ TaskManagement/TaskManagement.Tests/Commands.Tests/ShowAllTeamsTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManagement.Commands;
+using TaskManagement.Core;
+using TaskManagement.Core.Contracts;
+using TaskManagement.Models.Contracts;
+
+namespace TaskManagement.Tests.Commands.Tests
+{
+    [TestClass]
+    public class ShowAllTeamsTests
+    {
+        private const string teamName = "DummyTeam";
+
+        private IRepository repository;
+        private ICommandFactory commandFactory;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            this.repository = new Repository();
+            this.commandFactory = new CommandFactory(this.repository);
+        }
+
+        [TestMethod]
+        public void Execute_Should_ShowAllTeams()
+        {
+            //Arrange
+            var command = new ShowAllTeamsCommand(this.repository);
+            this.repository.CreateTeam("teamName");
+            //Act & Assert
+            Assert.IsTrue(command.Execute().Contains($"Number of all teams: {this.repository.Teams.Count}")
+                ,"Show all teams failed to return the right amount of teams!");
+        }
+
+        [TestMethod]
+        public void Execute_Should_ReturnNoTeamsMessage_When_TeamsListEmpty()
+        {
+            //Arrange
+            var command = new ShowAllTeamsCommand(this.repository);
+
+            //Act & Assert
+            Assert.IsTrue(command.Execute().Contains("There are no teams."));
+        }
+    }
+}
