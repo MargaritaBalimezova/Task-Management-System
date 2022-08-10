396807c4c66651a3af6a5945acbc73eff7a89abb
Angel Marinski <77447043+AngelMarinski@users.noreply.github.com>
2022-08-10 14:25:45 +0300

    bug fixed in RemoveMemberFromTeam command related to assigned tasks

--- TaskManagement/TaskManagement/Commands/RemoveMemberFromTeam.cs
+++ TaskManagement/TaskManagement/Commands/RemoveMemberFromTeam.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TaskManagement.Core.Contracts;
 using TaskManagement.Exceptions;
+using TaskManagement.Models.Tasks;
 using TaskManagement.Validations;
 
 namespace TaskManagement.Commands
@@ -26,10 +28,28 @@ namespace TaskManagement.Commands
             var team = this.Repository.FindTeamByName(CommandParameters[0]);
             var member = this.Repository.FindMemberByName(CommandParameters[1]);
 
+            var sb = new StringBuilder();
+            foreach (var task in member.Tasks)
+            {
+                member.RemoveTask(task);
+                sb.AppendLine($"\nTask with id {task.Id} is unassigned from {member.Name}.");
+                switch (task.GetType().Name)
+                {
+                    case "Bug":
+                        var bug = (Bug)task;
+                        bug.RemoveAssignee();
+                        break;
+
+                    case "Story":
+                        var story = (Story)task;
+                        story.RemoveAssignee();
+                        break;
+                }
+            }
+
             team.RemoveMember(member);
 
-            return $"Member with name : {member.Name} was successfully removed from team {team.Name}!";
-
+            return $"Member with name : {member.Name} was successfully removed from team {team.Name}!" + sb.ToString();
         }
     }
 }
